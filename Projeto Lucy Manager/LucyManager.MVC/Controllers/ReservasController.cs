53e9d365f399d1166af1f7951c8ad1bdf1fce229
MarcusVeloso <desenvolvedoroficial@gmail.com>
2016-03-08 22:07:10 -0300

    Create Controller/View Reservas

---
+++ Projeto Lucy Manager/LucyManager.MVC/Controllers/ReservasController.cs
@@ -0,0 +1,119 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using LucyManager.MVC.Models;
+
+namespace LucyManager.MVC.Controllers
+{
+    public class ReservasController : Controller
+    {
+        private LucyManagerDbContext db = new LucyManagerDbContext();
+
+        // GET: Reservas
+        public ActionResult Index()
+        {
+            return View(db.Reserva.ToList());
+        }
+
+        // GET: Reservas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Reservas reservas = db.Reserva.Find(id);
+            if (reservas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(reservas);
+        }
+
+        // GET: Reservas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Reservas/Create        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ReservaId,DataInicialReserva,DataFinalReserva,LocalId,UserId,EventoId")] Reservas reservas)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Reserva.Add(reservas);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(reservas);
+        }
+
+        // GET: Reservas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Reservas reservas = db.Reserva.Find(id);
+            if (reservas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(reservas);
+        }
+
+        // POST: Reservas/Edit/5        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ReservaId,DataInicialReserva,DataFinalReserva,LocalId,UserId,EventoId")] Reservas reservas)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(reservas).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(reservas);
+        }
+
+        // GET: Reservas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Reservas reservas = db.Reserva.Find(id);
+            if (reservas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(reservas);
+        }
+
+        // POST: Reservas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Reservas reservas = db.Reserva.Find(id);
+            db.Reserva.Remove(reservas);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
